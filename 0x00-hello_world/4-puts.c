3096d904820d918db29fe17142aecac2549a464d
joelmbithi <joellembithi@gmail.com>
2023-08-10 06:16:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc 
+#include <stdio.h>
+
+/*
+ * main - A program thats prints a line with  puts a  function
+ * Return:Always 0 succes
+ */
+
+int main(void)
+{
+puts ("\"Programming is like building a multilingual puzzle");
+return (0);
+}
