74f7a661669eb813d365887b8e77dbe8efd95b34
joelmbithi <joellembithi@gmail.com>
2023-08-10 07:23:34 -0700

    c programme

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
+#include <unistd.h>
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
